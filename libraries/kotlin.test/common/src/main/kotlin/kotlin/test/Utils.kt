a11b9dcf70698e6dcdddf01cefe6b5b0aeaab598
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-04-19 07:40:22 +0300

    Run kotlin-test-js tests with kotlin-stdlib-js
    Rework kotlin-test common tests to make them runnable with qunit.

--- libraries/kotlin.test/common/src/main/kotlin/kotlin/test/Utils.kt
+++ libraries/kotlin.test/common/src/main/kotlin/kotlin/test/Utils.kt
@@ -2,3 +2,11 @@ package kotlin.test
 
 internal fun messagePrefix(message: String?) = if (message == null) "" else "$message. "
 internal header fun lookupAsserter(): Asserter
+
+@PublishedApi // TODO: required until internal available in tests
+internal fun overrideAsserter(value: Asserter?): Asserter? {
+    // TODO: incorrect js generated: return _asserter.also { _asserter = value }
+    val previous = _asserter
+    _asserter = value
+    return previous
+}
