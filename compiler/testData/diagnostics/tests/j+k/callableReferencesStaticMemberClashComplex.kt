uujava/kotlin

commit b5a8ffaddc64b9862d8cf1173183df5e538e4c8e

Fix resolution of callable references
When there is unsuccessful (e.g invisible) result of one kind (static/non-static)
and there is a successful candidate for another kind, choose the latter one.

Note, that we have to postpone commiting trace until we choose one of the results,
otherwise errors of unsuccessful results are reported

TODO: Maybe it makes sense to report all results when all of them are
unsuccessful (NONE_APPLICABLE or something like this)

 #KT-16278 Fixed

compiler/frontend/src/org/jetbrains/kotlin/types/expressions/DoubleColonExpressionResolver.kt
@@ -58,9 +58,11 @@ import org.jetbrains.kotlin.types.typeUtil.isSubtypeOf
 import org.jetbrains.kotlin.types.typeUtil.makeNotNullable
 import org.jetbrains.kotlin.types.typeUtil.makeNullable
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+import org.jetbrains.kotlin.utils.yieldIfNotNull
 import java.lang.UnsupportedOperationException
 import java.util.*
 import javax.inject.Inject
+import kotlin.coroutines.experimental.buildSequence
 
 sealed class DoubleColonLHS(val type: KotlinType) {
     /**
@@ -626,15 +628,18 @@ class DoubleColonExpressionResolver(
         }
     }
 
+    private class ResolutionResultsAndTraceCommitCallback(
+            val results: OverloadResolutionResults<CallableDescriptor>,
+            val commitTrace: () -> Unit
+    )
+
     private fun tryResolveRHSWithReceiver(
             traceTitle: String,
             receiver: Receiver?,
             reference: KtSimpleNameExpression,
             outerContext: ResolutionContext<*>,
             resolutionMode: ResolveArgumentsMode
-    ): OverloadResolutionResults<CallableDescriptor>? {
-        checkReservedYield(reference, outerContext.trace)
-
+    ): ResolutionResultsAndTraceCommitCallback? {
         // we should preserve information about `call` because callable references are analyzed two times,
         // otherwise there will be not completed calls in trace
         val call = outerContext.trace[BindingContext.CALL, reference] ?: CallMaker.makeCall(reference, receiver, null, reference, emptyList())
@@ -652,12 +657,15 @@ class DoubleColonExpressionResolver(
                 reference, BasicCallResolutionContext.create(newContext, call, CheckArgumentTypesMode.CHECK_CALLABLE_TYPE)
         )
 
-        val shouldCommitTrace =
-                if (resolutionMode == ResolveArgumentsMode.SHAPE_FUNCTION_ARGUMENTS) resolutionResults.isSingleResult
-                else !resolutionResults.isNothing
-        if (shouldCommitTrace) temporaryTrace.commit()
-
-        return if (resolutionResults.isNothing) null else resolutionResults
+        return when {
+            resolutionResults.isNothing -> null
+            else -> ResolutionResultsAndTraceCommitCallback(resolutionResults) {
+                checkReservedYield(reference, outerContext.trace)
+                if (resolutionMode != ResolveArgumentsMode.SHAPE_FUNCTION_ARGUMENTS || resolutionResults.isSingleResult) {
+                    temporaryTrace.commit()
+                }
+            }
+        }
     }
 
     private fun resolveCallableReferenceRHS(
@@ -668,52 +676,71 @@ class DoubleColonExpressionResolver(
     ): OverloadResolutionResults<CallableDescriptor>? {
         val reference = expression.callableReference
 
-        val lhsType = lhs?.type ?:
-                      return tryResolveRHSWithReceiver("resolve callable reference with empty LHS", null, reference, c, mode)
+        val lhsType =
+                lhs?.type ?:
+                return tryResolveRHSWithReceiver("resolve callable reference with empty LHS", null, reference, c, mode)
+                       ?.apply { commitTrace() }?.results
 
-        when (lhs) {
-            is DoubleColonLHS.Type -> {
-                val classifier = lhsType.constructor.declarationDescriptor
-                if (classifier !is ClassDescriptor) {
-                    c.trace.report(CALLABLE_REFERENCE_LHS_NOT_A_CLASS.on(expression))
-                    return null
-                }
-
-                val qualifier = c.trace.get(BindingContext.QUALIFIER, expression.receiverExpression!!)
-                if (qualifier is ClassQualifier) {
-                    val possibleStatic = tryResolveRHSWithReceiver(
-                            "resolve unbound callable reference in static scope", qualifier, reference, c, mode
-                    )
-                    if (possibleStatic != null) return possibleStatic
-                }
-
-                val possibleWithReceiver = tryResolveRHSWithReceiver(
-                        "resolve unbound callable reference with receiver", TransientReceiver(lhsType), reference, c, mode
-                )
-                if (possibleWithReceiver != null) return possibleWithReceiver
-            }
-            is DoubleColonLHS.Expression -> {
-                val expressionReceiver = ExpressionReceiver.create(expression.receiverExpression!!, lhsType, c.trace.bindingContext)
-                val result = tryResolveRHSWithReceiver(
-                        "resolve bound callable reference", expressionReceiver, reference, c, mode
-                )
-                if (result != null) return result
-
-                if (lhs.isObjectQualifier) {
+        val resultSequence = buildSequence {
+            when (lhs) {
+                is DoubleColonLHS.Type -> {
                     val classifier = lhsType.constructor.declarationDescriptor
-                    val calleeExpression = expression.receiverExpression?.getCalleeExpressionIfAny()
-                    if (calleeExpression is KtSimpleNameExpression && classifier is ClassDescriptor) {
-                        val qualifier = ClassQualifier(calleeExpression, classifier)
-                        val possibleStatic = tryResolveRHSWithReceiver(
-                                "resolve object callable reference in static scope", qualifier, reference, c, mode
+                    if (classifier !is ClassDescriptor) {
+                        c.trace.report(CALLABLE_REFERENCE_LHS_NOT_A_CLASS.on(expression))
+                        return@buildSequence
+                    }
+
+                    val qualifier = c.trace.get(BindingContext.QUALIFIER, expression.receiverExpression!!)
+                    if (qualifier is ClassQualifier) {
+                        yieldIfNotNull(
+                                tryResolveRHSWithReceiver(
+                                        "resolve unbound callable reference in static scope", qualifier, reference, c, mode
+                                )
                         )
-                        if (possibleStatic != null) return possibleStatic
+                    }
+
+                    yieldIfNotNull(
+                            tryResolveRHSWithReceiver(
+                                    "resolve unbound callable reference with receiver", TransientReceiver(lhsType), reference, c, mode
+                            )
+                    )
+                }
+                is DoubleColonLHS.Expression -> {
+                    val expressionReceiver = ExpressionReceiver.create(expression.receiverExpression!!, lhsType, c.trace.bindingContext)
+                    yieldIfNotNull(
+                            tryResolveRHSWithReceiver(
+                                    "resolve bound callable reference", expressionReceiver, reference, c, mode
+                            )
+                    )
+
+                    if (lhs.isObjectQualifier) {
+                        val classifier = lhsType.constructor.declarationDescriptor
+                        val calleeExpression = expression.receiverExpression?.getCalleeExpressionIfAny()
+                        if (calleeExpression is KtSimpleNameExpression && classifier is ClassDescriptor) {
+                            val qualifier = ClassQualifier(calleeExpression, classifier)
+                            yieldIfNotNull(
+                                    tryResolveRHSWithReceiver(
+                                            "resolve object callable reference in static scope", qualifier, reference, c, mode
+                                    )
+                            )
+                        }
                     }
                 }
             }
         }
 
-        return null
+        // TODO: Maybe it makes sense to report all results when all of them are unsuccessful (NONE_APPLICABLE or something like this)
+        var resultToCommit: ResolutionResultsAndTraceCommitCallback? = null
+        for (result in resultSequence) {
+            resultToCommit = result
+            if (result.results.isSuccess) {
+                break
+            }
+        }
+        return resultToCommit?.let {
+            it.commitTrace()
+            it.results
+        }
     }
 
     companion object {

compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -2199,6 +2199,12 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                     doTest(fileName);
                 }
 
+                @TestMetadata("privateStaticAndPublicMember.kt")
+                public void testPrivateStaticAndPublicMember() throws Exception {
+                    String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/callableReference/function/privateStaticAndPublicMember.kt");
+                    doTest(fileName);
+                }
+
                 @TestMetadata("renameOnImport.kt")
                 public void testRenameOnImport() throws Exception {
                     String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/callableReference/function/renameOnImport.kt");

compiler/testData/diagnostics/tests/callableReference/function/privateStaticAndPublicMember.kt
@@ -1,0 +1,10 @@
+// SKIP_TXT
+// FILE: A.java
+class A {
+    private static void foo() {}
+    public String foo(int x) {}
+}
+// FILE: main.kt
+fun main() {
+    (A::foo)(A(), 1)
+}

core/util.runtime/src/org/jetbrains/kotlin/utils/collections.kt
@@ -17,6 +17,7 @@
 package org.jetbrains.kotlin.utils
 
 import java.util.*
+import kotlin.coroutines.experimental.SequenceBuilder
 
 fun <K, V> Iterable<K>.keysToMap(value: (K) -> V): Map<K, V> {
     return associateBy({ it }, value)
@@ -62,6 +63,8 @@ fun <T: Any> emptyOrSingletonList(item: T?): List<T> = listOfNotNull(item)
 fun <T: Any> MutableCollection<T>.addIfNotNull(t: T?) {
     if (t != null) add(t)
 }
+
+suspend fun <T: Any> SequenceBuilder<in T>.yieldIfNotNull(t: T?) = if (t != null) yield(t) else Unit
 
 fun <K, V> newHashMapWithExpectedSize(expectedSize: Int): HashMap<K, V> =
         HashMap(capacity(expectedSize))

commit 97a4de9aa2d5de308b7de898ebb0b03d29c2126c

Fix callable reference resolution regression
The regression appeared after
b5a8ffaddc64b9862d8cf1173183df5e538e4c8e
when we started trying both static and member methods until
first success and when there is no successful
we were just leaving the last one (e.g. private member)

But the actual problem is that we were commiting the trace
in case of single (but incorrect) result in resolution mode
SHAPE_FUNCTION_ARGUMENTS when we couldn't yet choose the
correct static method

Another problem has been fixed: we shouldn't assume
the shape of callable reference is single, but unsuccessful
result as it may lead to wrong decision when choosing
candidates for the outer call

 #KT-17597 Fixed

compiler/frontend/src/org/jetbrains/kotlin/resolve/calls/ArgumentTypeResolver.java
@@ -265,7 +265,7 @@ public class ArgumentTypeResolver {
 
         if (overloadResolutionResults == null) return null;
 
-        if (overloadResolutionResults.isSingleResult()) {
+        if (overloadResolutionResults.isSuccess()) {
             ResolvedCall<?> resolvedCall =
                     OverloadResolutionResultsUtil.getResultingCall(overloadResolutionResults, context.contextDependency);
             if (resolvedCall == null) return null;

compiler/frontend/src/org/jetbrains/kotlin/types/expressions/DoubleColonExpressionResolver.kt
@@ -665,7 +665,7 @@ class DoubleColonExpressionResolver(
             resolutionResults.isNothing -> null
             else -> ResolutionResultsAndTraceCommitCallback(resolutionResults) {
                 checkReservedYield(reference, outerContext.trace)
-                if (resolutionMode != ResolveArgumentsMode.SHAPE_FUNCTION_ARGUMENTS || resolutionResults.isSingleResult) {
+                if (resolutionMode != ResolveArgumentsMode.SHAPE_FUNCTION_ARGUMENTS || resolutionResults.isSuccess) {
                     temporaryTrace.commit()
                 }
             }

compiler/testData/diagnostics/tests/j+k/callableReferencesStaticMemberClash.kt
@@ -1,0 +1,16 @@
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+// FILE: A.java
+public class A {
+    public static void foo() {}
+    public static void foo(String x) {}
+
+    private void foo(int y) {}
+}
+
+// FILE: main.kt
+
+fun baz(x: (String) -> Unit) {}
+
+fun bar() {
+    baz(A::foo)
+}

compiler/testData/diagnostics/tests/j+k/callableReferencesStaticMemberClashComplex.kt
@@ -1,0 +1,24 @@
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+// !CHECK_TYPE
+// FILE: A.java
+public class A {
+    public static void foo(String x) {}
+    public static void foo(A a, int e) {}
+    private void foo(int y) {}
+}
+
+// FILE: main.kt
+
+interface X
+interface Y
+object Z : X, Y
+
+fun baz(x: (String) -> Unit, y: X) = 1
+fun baz(x: (A, Int) -> Unit, y: Y) = ""
+
+fun bar() {
+    // TODO: there should be a resolution ambiguity (and it's reported to the trace, but it gets lost somewhere)
+    // To prove that there is ambiguity there is a calls to both `String` and `Int` methods and neither of them is resolved
+    baz(A::<!INVISIBLE_MEMBER!>foo<!>, Z).<!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>length<!>
+    baz(A::<!INVISIBLE_MEMBER!>foo<!>, Z).<!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>toDouble<!>()
+}

compiler/testData/diagnostics/testsWithJava8/patternCompileCallableReference.kt
@@ -1,0 +1,6 @@
+// SKIP_TXT
+import java.util.regex.Pattern
+
+val strs: List<String> = listOf("regex1", "regex2")
+
+val patterns: List<Pattern> = strs.map(Pattern::compile)

compiler/tests-java8/tests/org/jetbrains/kotlin/checkers/DiagnosticsWithJava8TestGenerated.java
@@ -60,6 +60,12 @@ public class DiagnosticsWithJava8TestGenerated extends AbstractDiagnosticsWithFu
         doTest(fileName);
     }
 
+    @TestMetadata("patternCompileCallableReference.kt")
+    public void testPatternCompileCallableReference() throws Exception {
+        String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/testsWithJava8/patternCompileCallableReference.kt");
+        doTest(fileName);
+    }
+
     @TestMetadata("samWithConsumer.kt")
     public void testSamWithConsumer() throws Exception {
         String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/testsWithJava8/samWithConsumer.kt");

compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -11818,6 +11818,18 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("callableReferencesStaticMemberClash.kt")
+            public void testCallableReferencesStaticMemberClash() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/callableReferencesStaticMemberClash.kt");
+                doTest(fileName);
+            }
+
+            @TestMetadata("callableReferencesStaticMemberClashComplex.kt")
+            public void testCallableReferencesStaticMemberClashComplex() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/callableReferencesStaticMemberClashComplex.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("canDeclareIfSamAdapterIsInherited.kt")
             public void testCanDeclareIfSamAdapterIsInherited() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/canDeclareIfSamAdapterIsInherited.kt");